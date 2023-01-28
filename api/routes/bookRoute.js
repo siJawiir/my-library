60c22112627ab570a1b154b582c3711a61f708ee
LesGrooper <eqimulia@ymail.com>
2023-01-28 17:15:26 +0700

    adding multer to middleware and config to router

--- api/routes/bookRoute.js
+++ api/routes/bookRoute.js
@@ -1,9 +1,10 @@
 const bookRoute = require("express").Router();
 const BookController = require("../controllers/BookController");
+const  multer  = require("../middleware/multer");
 
 bookRoute.get("/", BookController.getData);
 bookRoute.get("/book/:id", BookController.getDataBy);
-bookRoute.post("/", BookController.postData);
+bookRoute.post("/", multer.upload.single('image'), BookController.postData);
 bookRoute.put("/update/:id", BookController.putData);
 bookRoute.delete("/delete/:id", BookController.deleteData);
 
