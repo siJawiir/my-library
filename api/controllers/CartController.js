b9a4b9a2587aa41c160941e4b725a426f867350f
LesGrooper <eqimulia@ymail.com>
2023-02-04 00:42:55 +0700

    some bugs on cart

--- api/controllers/CartController.js
+++ api/controllers/CartController.js
@@ -15,6 +15,9 @@ class CartController {
           },
         ],
       });
+      if (!data) {
+        resp.status(400).json("Bad Request");
+      }
       resp.status(200).json(data);
     } catch (error) {
       resp.status(500).json(error);
@@ -33,18 +36,57 @@ class CartController {
 
   static async postData(req, resp) {
     try {
-      const {book_id, user_id, quantity} = req.body
-      const data = await Cart.create({
-         book_id, user_id, quantity
-      })
-      data
-         ? resp.status(200).json({
-             message: "Cart ID has Added!",
-             data,
-           })
-         : resp.status(403).json({
-             message: "Data ID Coulnd't be Added!",
-           });
+      const user = req.user;
+      const { book_id, quantity } = req.body;
+      const cart = await Cart.findOne({ where: user });
+      const book = await Book.findOne({
+        where: {
+          id: book_id,
+        },
+      });
+
+      // console.log(book.dataValues)
+
+      if (!book) {
+        resp.status(404).json({
+          MessageChannel: "Item not Found!",
+        });
+        return;
+      }
+
+      const price = book.price;
+      const title = book.title;
+
+      if (cart) {
+        const bookIndex = cart.books?.findIndex(
+          (book) => book.book_id == book_id
+        );
+        // chek if book exist
+        if (bookIndex > -1) {
+          let product = cart.books[bookIndex];
+          product.quantity += quantity;
+          cart.quantity = cart.books.reduce((acc, curr) => {
+            return acc + curr.quantity * curr.price;
+          }, 0);
+          cart.books[bookIndex] = product;
+          await cart.save();
+          resp.status(200).send(cart);
+        } else {
+          cart.books?.push({ books_id, title, quantity, price });
+          cart.quantity = cart.books?.reduce((acc, curr) => {
+            return acc + curr.quantity * curr.price;
+          }, 0);
+          await cart.save();
+          resp.status(200).json(cart);
+        }
+      } else {
+        // check cart exist, create
+        const newCart = await Cart.create({
+          user,
+          books: [{ book_id, title, quantity, price }],
+        });
+        return resp.status(200).json(newCart);
+      }
 
       // const book_id = req.body.id;
       // let fetchedCart;
@@ -79,16 +121,38 @@ class CartController {
       //       }
       //    })
       // })
-      
     } catch (error) {
       resp.status(500).json(error);
-      console.log(error)
+      console.log(error);
     }
   }
 
   static async deleteData(req, resp) {
     try {
-    } catch (error) {}
+      const user = req.user;
+      const id = +req.params.id;
+      let cart = await Cart.findOne({ user });
+      const bookIndex = cart.books.findIndex((book) => book.book_id == book_id);
+      if (bookIndex > - 1 ) {
+        let book = cart.books[bookIndex]
+        cart.quantity -= book.quantity * book.price
+        if (cart.quantity < 0) {
+          cart.quantity = 0
+        }
+        cart.books.splice(bookIndex, 1)
+        cart.quantity = cart.books.reduce((acc, curr) => {
+          return acc+ curr.quantity * curr.price
+        },0)
+        cart = await cart.save()
+        resp.status(200).json(cart)
+      }else{
+        resp.status(404).json({
+          MessageChannel: "item not found!"
+        })
+      }
+    } catch (error) {
+      resp.status(500).json(error)
+    }
   }
 }
 
